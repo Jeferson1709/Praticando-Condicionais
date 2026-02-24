4c3802d7dbcab3eb5c2185175abd48bb9bf992bc
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-02-24 18:15:56 -0300

    feat: Ternários e Switch Case

--- exercicios.js
+++ exercicios.js
@@ -45,8 +45,75 @@ const ano = 1995;
 // const ano = 2022;
 // const ano = 1992;
 
-if (((ano % 4 === 0) && (ano % 100 !== 0)) || (ano % 400 === 0)) {
+if ((ano % 4 === 0 && ano % 100 !== 0) || ano % 400 === 0) {
   console.log(`${ano} é bissexto`);
 } else {
   console.log(`${ano} não é bissexto`);
+}
+
+// Operador ternário/ operador condicional
+
+// function saudar(nome) {
+//   console.log(`olá ${nome}`);
+// }
+// nome ? saudar(nome) : saudar(pessoa);
+// const nome = "Jeferson";
+// const saudacao = nome ? `olá ${nome}` : `olá pessoa`;
+// console.log(saudacao);
+
+// SWITCH /CASE
+const avaliacao = 2;
+switch (avaliacao) {
+  case 5:
+    console.log("avaliação ótima");
+    break;
+  case 4:
+    console.log("avaliação boa");
+    break;
+  case 3:
+    console.log("avaliação neutra");
+    break;
+  default:
+    console.log("avaliação baixa");
+    break;
+}
+
+//OP CONDICIONAL E SWITCH
+// Verificar se estudante receberá bônus na nota.
+// Estudantes recebem bônus se nota for 8 ou acima e se tiverem no máximo 2 faltas.
+
+const nomeAlu = "Roberta";
+const nota = 8;
+const faltas = 2;
+
+const recebeBonus =
+  nota >= 8 && faltas <= 2
+    ? `${nomeAlu} recebe bônus`
+    : `${nomeAlu} não recebe bônus`;
+
+console.log(recebeBonus);
+
+// criar um fluxo que identifica o tipo de pessoa usuária e comunica de acordo.
+// ex: pessoa usuária free tem acesso limitado ao app
+//     pessoa usuária premium tem acesso a todas as funções
+//     pessoa usuária super premium tem acesso total e bônus especiais
+
+const user = "premium";
+
+switch (user) {
+  case "free":
+    console.log("Acesso limitado");
+    break;
+  case "premium":
+    console.log("Acesso total ao app");
+    break;
+  case "super premium":
+    console.log("Acesso total ao app e bônus");
+
+    break;
+
+  default:
+    console.log("tipo de user desconhecido");
+
+    break;
 }
