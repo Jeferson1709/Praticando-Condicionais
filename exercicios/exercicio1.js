ee4c2bbf1ea3771b9d058f2ae2ea00582388c2eb
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-02-25 17:25:04 -0300

    feat: Exercícios de condicionais.

---
+++ exercicios/exercicio1.js
@@ -0,0 +1,11 @@
+/* Você está desenvolvendo um sistema de controle de acesso para um evento. Para entrar, a pessoa precisa ter 18 anos ou mais.
+
+Crie um programa que, dado o valor da idade, verifique se a pessoa pode ou não entrar no evento. Exiba a mensagem informando se o acesso foi ou não permitido */
+
+const idade = 18;
+
+if (idade > 17) {
+  console.log("Acesso permitido!   ");
+} else {
+  console.log("Acesso negado!");
+}
---
+++ exercicios/exercicio2.js
@@ -0,0 +1,16 @@
+/* Você está desenvolvendo um sistema meteorológico para informar sobre o clima do dia. Com base na temperatura, o sistema deve classificar o dia conforme os critérios abaixo:
+
+Menor que 15: "Frio"
+De 15 a 25: "Agradável"
+Acima de 25: "Quente"
+Escreva um programa que, dado um valor de temperatura e imprima a mensagem correspondente. */
+
+const temperatura = 25;
+
+if (temperatura < 15) {
+  console.log("Frio");
+} else if (temperatura < 26) {
+  console.log("Agradável");
+} else {
+  console.log("Quente");
+}
